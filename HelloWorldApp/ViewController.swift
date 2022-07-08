271bd6b5a6e76068ea97524700076f48a247e503
Alexey Suslov <suslov.aleksey.oo7@gmail.com>
2022-07-08 20:08:49 +0300

    Initial Commit

---
+++ HelloWorldApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorldApp
+//
+//  Created by ALEKSEY SUSLOV on 08.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
